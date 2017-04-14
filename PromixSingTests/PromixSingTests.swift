0029b6d71f7af6b52b29f27ef6a1bc2726a9ac20
Hyunsoo Park <qkrgustnrk@naver.com>
2017-04-14 01:51:34 -0700

    Initial Commit

---
+++ PromixSingTests/PromixSingTests.swift
@@ -0,0 +1,36 @@
+//
+//  PromixSingTests.swift
+//  PromixSingTests
+//
+//  Created by Hyunsoo Park on 4/14/17.
+//  Copyright © 2017 Hyunsoo Park. All rights reserved.
+//
+
+import XCTest
+@testable import PromixSing
+
+class PromixSingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
